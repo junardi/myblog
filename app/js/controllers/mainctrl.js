d612b4fc898bfce0da2ffc2ec3ea9bfc81040ed2
Mark Boribor <mark.b@projectassistant.org>
2017-09-12 05:02:15 +0800

    Learn push data

--- app/js/controllers/mainctrl.js
+++ app/js/controllers/mainctrl.js
@@ -1,12 +1,31 @@
-module.exports = function($scope, $firebaseRef, $firebaseArray) {
-	$scope.test = "Testing...";
+module.exports = function($scope, $firebaseRef, $firebaseArray, $firebaseObject) {
+	
+	var usersdataRef = $firebaseRef.usersdata;    
+	$scope.usersdata = $firebaseObject(usersdataRef);            
+	console.log($scope.usersdata);      
 
-	$scope.hello = "Hello World!";
+	$scope.usersdata.$loaded().then(function(data) {
+		//console.log(data === obj); // true   
+		console.log("Users data are loaded");
+	}).catch(function(error) {
+		console.error("Error:", error);
+	});   
 
-	var itemsRef = $firebaseRef.items;    
+	var unwatch = $scope.usersdata.$watch(function() {
+	  console.log("usersdata changed!");
+	});
 
-	$scope.items = $firebaseArray(itemsRef);            
 
-	console.log($scope.items);
+	$scope.item = "Value";   
+	$scope.addItem = function() { 
+		console.log("Let's add an item");
+	};       
+
+	var itemsRef = $firebaseRef.items;
+	var itemsArray = { 
+		name: "First item"
+	};
+	itemsRef.push(itemsArray);
+
 
 };
--- app/js/main.js
+++ app/js/main.js
@@ -6,23 +6,23 @@ require('angularfire');
 
 var config = {};
 
-// config = {
-//      apiKey: "AIzaSyD2GZ7I0ZaWK2Z16fm8CLqvqb3DiS3UIj8",
-//      authDomain: "myblog-80f2e.firebaseapp.com",
-//      databaseURL: "https://myblog-80f2e.firebaseio.com",
-//      projectId: "myblog-80f2e",
-//      storageBucket: "myblog-80f2e.appspot.com",
-//      messagingSenderId: "515114687685"
-// };   
-
 config = {
-     apiKey: "AIzaSyAlfI9DmnwF0S9eRULfKAOWBKiz9GY2bu0",
-     authDomain: "practice-157b9.firebaseapp.com",
-     databaseURL: "https://practice-157b9.firebaseio.com",
-     projectId: "practice-157b9",
-     storageBucket: "practice-157b9.appspot.com",
-     messagingSenderId: "1002831516501"
-};
+     apiKey: "AIzaSyD2GZ7I0ZaWK2Z16fm8CLqvqb3DiS3UIj8",
+     authDomain: "myblog-80f2e.firebaseapp.com",
+     databaseURL: "https://myblog-80f2e.firebaseio.com",
+     projectId: "myblog-80f2e",
+     storageBucket: "myblog-80f2e.appspot.com",
+     messagingSenderId: "515114687685"
+};   
+
+// config = {
+//      apiKey: "AIzaSyAlfI9DmnwF0S9eRULfKAOWBKiz9GY2bu0",
+//      authDomain: "practice-157b9.firebaseapp.com",
+//      databaseURL: "https://practice-157b9.firebaseio.com",
+//      projectId: "practice-157b9",
+//      storageBucket: "practice-157b9.appspot.com",
+//      messagingSenderId: "1002831516501"
+// };
 
 firebase.initializeApp(config);
 
@@ -43,7 +43,8 @@ app.config( function($locationProvider, $routeProvider, $firebaseRefProvider, gl
      $locationProvider.hashPrefix(''); 
    
      $firebaseRefProvider.registerUrl({
-          default: config.databaseURL,  
+          default: config.databaseURL,   
+          usersdata: config.databaseURL + "/details", 
           items: config.databaseURL + "/items"     
      });
      // routes
@@ -62,5 +63,5 @@ app.config( function($locationProvider, $routeProvider, $firebaseRefProvider, gl
 });
 
 //Load controller
-app.controller('MainController', ['$scope', '$firebaseRef', '$firebaseArray', '$location', mainCtrl]);   
-app.controller('HomeController', ['$scope', '$firebaseRef', '$firebaseArray', '$location', homeCtrl]);
+app.controller('MainController', ['$scope', '$firebaseRef', '$firebaseArray', '$firebaseObject', '$location', mainCtrl]);   
+app.controller('HomeController', ['$scope', '$firebaseRef', '$firebaseArray', '$firebaseObject', '$location', homeCtrl]);
